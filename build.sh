74be65f404c66133854e07d4fd3a859081519831
Stone749990226 <749990226@qq.com>
2025-06-25 16:18:49 +0800

    add dependence/libxcrypt

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-export CFLAGS="-UHAVE_UTIME_H -DHAVE_SOCKETPAIR=0 -DDISABLE_UTMP -DDISABLE_UTMPX -DDISABLE_WTMP -DDISABLE_WTMPX -DNO_UTC_OFFSET -I/home/stone/os/rux-openssh/openssh/libxcrypt/aarch64-libcrypt-install/include -g -O0"
+export CFLAGS="-UHAVE_UTIME_H -DHAVE_SOCKETPAIR=0 -DDISABLE_UTMP -DDISABLE_UTMPX -DDISABLE_WTMP -DDISABLE_WTMPX -DNO_UTC_OFFSET -I$(pwd)/dependence/aarch64-libcrypt-install/include -g -O0"
 export CC=aarch64-linux-musl-gcc
 export AR=aarch64-linux-musl-ar
 export RANLIB=aarch64-linux-musl-ranlib
-export LDFLAGS="-static -L/home/stone/os/rux-openssh/openssh/libxcrypt/aarch64-libcrypt-install/lib -lcrypt"
+export LDFLAGS="-static -L$(pwd)/dependence/aarch64-libcrypt-install/lib -lcrypt"
 
 make clean
 
@@ -23,6 +23,6 @@ make -j$(nproc)
 mv sshd ./output
 mv sshd-auth ./output
 mv sshd-session ./output
-cp ./output/sshd /home/stone/fork/ruxos/apps/c/openssh/rootfs/bin
-cp ./output/sshd-auth /home/stone/fork/ruxos/apps/c/openssh/rootfs/usr/local/libexec
-cp ./output/sshd-session /home/stone/fork/ruxos/apps/c/openssh/rootfs/usr/local/libexec
+# cp ./output/sshd /home/stone/fork/ruxos/apps/c/openssh/rootfs/bin
+# cp ./output/sshd-auth /home/stone/fork/ruxos/apps/c/openssh/rootfs/usr/local/libexec
+# cp ./output/sshd-session /home/stone/fork/ruxos/apps/c/openssh/rootfs/usr/local/libexec
